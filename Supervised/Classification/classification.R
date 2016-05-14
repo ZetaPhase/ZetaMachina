18380a225548e96b1e02ea0c07410389437fb9ba
Dave Ho <daveho8888@gmail.com>
2016-05-14 16:41:17 -0700

    added samples

---
+++ Supervised/Classification/classification.R
@@ -0,0 +1,4 @@
+#sample(x, size, replace = False, prob = NULL)
+probability = c("Bronze", "Bronze", "Bronze", "Black", "Red", "Red", "Brown", "Brown", "Brown", "Brown")
+samples = sample(probability, 100, replace=TRUE)
+samples
