0987cefe0d57beef728c420a0d7b180d52c2095e
Dave Ho <daveho8888@gmail.com>
2016-05-14 19:17:23 -0700

    add tons of stuff

--- Supervised/Classification/classification.R
+++ Supervised/Classification/classification.R
@@ -5,6 +5,8 @@
 library(e1071)
 library(dplyr)
 peopleCount <- 500
+count <- 1:1000
+ind <- sample(count, 800, replace=FALSE)
 hairColor <- sapply(list("Bronze", "Black", "Red", "Brown"), length)
 race <- sapply(list("Asian", "European", "Latino"), length)
 maleHair <- sample(c("Bronze", "Black", "Red", "Brown"),
@@ -26,5 +28,13 @@ femaleRace <- sample(c("Asian", "European", "Latino"),
                     replace = TRUE)
 female <- data.frame(Hair=femaleHair, Race=femaleRace, Gender="F")
 people <- rbind(male, female)
-model <- naiveBayes(Gender ~ ., data=people)
-filter(people, Hair=="Black", Gender=="M")
+training <- people[ind,]
+testing <- people[-ind,]
+model <- naiveBayes(Gender ~ ., data=training)
+tmp <- people %>% filter(Hair=="Black" & Gender=="M")
+groupdf <- people %>% group_by(Gender, Hair) %>% summarise(count=n())
+groupdf$Prob=groupdf$count/500
+
+daveNaiveBayes <- function(input, output, data){
+  
+}
