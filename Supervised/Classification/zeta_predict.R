a1bc9fff6d9ff22c0e3ec62b1bc094e985e7e700
Dave Ho <daveho8888@gmail.com>
2016-05-17 21:51:03 -0700

    zeta_predict.R self implementation created
    severe cleaning up

---
+++ Supervised/Classification/zeta_predict.R
@@ -0,0 +1,40 @@
+library(e1071)
+library(dplyr)
+peopleCount <- 500
+hairColor <- sapply(list("Bronze", "Black", "Red", "Brown"), length)
+race <- sapply(list("Asian", "European", "Latino"), length)
+maleHair <- sample(c("Bronze", "Black", "Red", "Brown"),
+                   size = peopleCount,
+                   prob = rep(c(0.3, 0.1, 0.2, 0.4) / hairColor, hairColor),
+                   replace = TRUE)
+maleRace <- sample(c("Asian", "European", "Latino"),
+                   size = peopleCount,
+                   prob = rep(c(0.3, 0.6, 0.1) / race, race),
+                   replace = TRUE)
+male <- data.frame(Hair=maleHair, Race=maleRace, Gender="M")
+femaleHair <- sample(c("Bronze", "Black", "Red", "Brown"),
+                     size = peopleCount,
+                     prob = rep(c(0.05, 0.5, 0.3, 0.15) / hairColor, hairColor),
+                     replace = TRUE)
+femaleRace <- sample(c("Asian", "European", "Latino"),
+                     size = peopleCount,
+                     prob = rep(c(0.2, 0.1, 0.7) / race, race),
+                     replace = TRUE)
+female <- data.frame(Hair=femaleHair, Race=femaleRace, Gender="F")
+people <- rbind(male, female)
+
+daveNaiveBayes <- function(input, output, data){
+  outputProb <- c()
+  cmd_str  <- paste("data %>% group_by(", output, ") %>% summarise(count=n())", sep="")
+  outputdf <- eval(parse(text=cmd_str))
+  outputdf$Prob=outputdf$count/nrow(data)
+  outputTable <- matrix(unlist(outputdf["Prob"]), ncol=nrow(outputdf), byrow=TRUE)
+  colnames(outputTable) <- unlist(outputdf["Gender"])
+  outputTable <- as.table(outputTable)
+  outputProb <- c(outputProb, outputdf[outputdf$Gender=="M",]$Prob)
+  for (out in output){
+    
+  }
+  #groupdf <- people %>% group_by(Gender, Hair, Race) %>% summarise(count=n())
+  #groupdf$Prob=groupdf$count/500
+}
